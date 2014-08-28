5938a23aba8798b34ceb617e32fe43fa36114761
John.Koepi <john.koepi@gmail.com>
2014-08-28 15:12:07 +0300

    SPY-121: Make NOOP send optional on connect.
    This changeset makes the NOOP sending before AUTH
    on connect optional, because this may have issues
    with other systems and therefore lead to non-working
    systems.
    
    The check can be activated by setting the
    net.spy.verifyAliveOnConnect system property to
    true.
    
    Change-Id: If26439c024c3c50f02e992aaa80ba39fa683aed0
    Reviewed-on: http://review.couchbase.org/26151
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Reviewed-by: Deepti Dawar <deepti.dawar@globallogic.com>
    Tested-by: Michael Nitschinger <michael.nitschinger@couchbase.com> (cherry-picking from commit b90bc111c59e7c37038ef8a36dc12cb6ca42db15)

--- src/main/java/net/spy/memcached/MemcachedConnection.java
+++ src/main/java/net/spy/memcached/MemcachedConnection.java
@@ -104,6 +104,7 @@ public class MemcachedConnection extends SpyThread {
   private final int timeoutExceptionThreshold;
   private final Collection<Operation> retryOps;
   protected final ConcurrentLinkedQueue<MemcachedNode> nodesToShutdown;
+  private final boolean verifyAliveOnConnect;
 
   /**
    * Construct a memcached connection.
@@ -130,6 +131,14 @@ public class MemcachedConnection extends SpyThread {
     nodesToShutdown = new ConcurrentLinkedQueue<MemcachedNode>();
     this.bufSize = bufSize;
     this.connectionFactory = f;
+
+    String verifyAlive = System.getProperty("net.spy.verifyAliveOnConnect");
+    if (verifyAlive != null && verifyAlive.equals("true")) {
+      verifyAliveOnConnect = true;
+    } else {
+      verifyAliveOnConnect = false;
+    }
+
     List<MemcachedNode> connections = createConnections(a);
     locator = f.createLocator(connections);
     setName("Memcached IO over " + this);
@@ -410,48 +419,49 @@ public class MemcachedConnection extends SpyThread {
         getLogger().info("Connection state changed for %s", sk);
         final SocketChannel channel = node.getChannel();
         if (channel.finishConnect()) {
+          if(verifyAliveOnConnect) {
+            // Test to see if it's truly alive, could be a hung process, OS
+            final CountDownLatch latch = new CountDownLatch(1);
+            final OperationFuture<Boolean> rv =
+              new OperationFuture<Boolean>("noop", latch, 2500);
+            final NoopOperation testOp = opFact.noop(new OperationCallback() {
+              public void receivedStatus(Operation operation, OperationStatus status) {
+                rv.set(status.isSuccess(), status);
+              }
 
-          // Test to see if it's truly alive, could be a hung process, OS
-          final CountDownLatch latch = new CountDownLatch(1);
-          final OperationFuture<Boolean> rv =
-            new OperationFuture<Boolean>("noop", latch, 2500);
-          final NoopOperation testOp = opFact.noop(new OperationCallback() {
-            public void receivedStatus(Operation operation, OperationStatus status) {
-              rv.set(status.isSuccess(), status);
-            }
+              @Override
+              public void complete(Operation operation) {
+                latch.countDown();
+              }
+            });
 
-            @Override
-            public void complete(Operation operation) {
-              latch.countDown();
-            }
-          });
+            testOp.setHandlingNode(node);
+            testOp.initialize();
 
-          testOp.setHandlingNode(node);
-          testOp.initialize();
+            checkState();
+            insertOperation(node, testOp);
+            node.copyInputQueue();
 
-          checkState();
-          insertOperation(node, testOp);
-          node.copyInputQueue();
-
-          boolean done = false;
-          if (sk.isValid()) {
-            long timeout = TimeUnit.MILLISECONDS.toNanos(
-              connectionFactory.getOperationTimeout());
-            for (long stop = System.nanoTime() + timeout;
-              stop > System.nanoTime();) {
-              handleWrites(sk, node);
-              handleReads(sk, node);
-              if (done = (latch.getCount() == 0)) {
-                break;
+            boolean done = false;
+            if (sk.isValid()) {
+              long timeout = TimeUnit.MILLISECONDS.toNanos(
+                connectionFactory.getOperationTimeout());
+              for (long stop = System.nanoTime() + timeout;
+                stop > System.nanoTime();) {
+                handleWrites(sk, node);
+                handleReads(sk, node);
+                if (done = (latch.getCount() == 0)) {
+                  break;
+                }
               }
             }
-          }
 
-          if (!done || testOp.isCancelled() || testOp.hasErrored()
-            || testOp.isTimedOut()) {
-            throw new ConnectException("Could not send noop upon connect! "
-              + "This may indicate a running, but not responding memcached "
-              + "instance.");
+            if (!done || testOp.isCancelled() || testOp.hasErrored()
+              || testOp.isTimedOut()) {
+              throw new ConnectException("Could not send noop upon connect! "
+                + "This may indicate a running, but not responding memcached "
+                + "instance.");
+            }
           }
 
           connected(node);
